f799ca574002143f7a74507b29e245d9865afa8e
Hilkias <43548850+Hilkias@users.noreply.github.com>
2018-11-29 14:26:06 -0700

    sss

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -1,32 +1,51 @@
-//'use strict';
-
-
-
-
-
-// Gets longitude and latitude of current location
-function getGeoLocation() {
-
-} // end getGeoLocation
-
-const STATUS = document.getElementById('status');
-STATUS.innerHTML = 'Getting Location...';
+'use strict';
 
 // Call the function to get our location
 getGeoLocation();
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-     const LAT = position.coords.latitude;
-     const LONG = position.coords.longitude;
-  
-     // Combine the values
-     const LOCALE = LAT + "," + LONG;
-     console.log(`Lat and Long are: ${LOCALE}.`);
-  
-  
-    })
-   } else {
-    STATUS.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
-   } // end else
-  } //end getGeoLocation
+// Gets longitude and latitude of current location
+function getGeoLocation() {
+    const STATUS = document.getElementById('status');
+    STATUS.innerHTML = 'Getting Location...';
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const LAT = position.coords.latitude;
+            const LONG = position.coords.longitude;
+
+            // Combine the values
+            const LOCALE = LAT + "," + LONG;
+            console.log(`Lat and Long are: ${LOCALE}.`);
+
+
+            // Call getCode function, send locale
+            getCode(LOCALE);
+        })
+    } else {
+        STATUS.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
+    } // end else
+} //end getGeoLocation
+
+// URL to request city data using latitude and longitude
+// "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=WZvTdK3lWnFfx2XJNtJCbcPzebd1h19y&q=43.816667%2C-111.783333"
+// Get location code from API
+function getCode(LOCALE) {
+    const API_KEY = 'WZvTdK3lWnFfx2XJNtJCbcPzebd1h19y';
+    const URL = 'ttps://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+API_KEY+'&q='+LOCALE;
+    fetch(URL)
+     .then(response => response.json())
+     .then(function (data) {
+      console.log('Json object from getCode function:');
+      console.log(data);
+      const locData = {}; // Create an empty object
+      locData['key'] = data.Key; // Add the value to the object
+      locData['name'] = data.LocalizedName;
+      locData['postal'] = data.PrimaryPostalCode;
+      locData['state'] = data.AdministrativeArea.LocalizedName;
+      locData['stateAbbr'] = data.AdministrativeArea.ID;
+      locData['geoposition'] = LOCALE;
+      locData['elevation'] = data.GeoPosition.Elevation.Imperial.Value;
+      getWeather(locData);
+      })
+     .catch(error => console.log('There was a getCode error: ', error))
+  } // end getCode function
